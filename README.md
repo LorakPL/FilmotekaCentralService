1c9000dbd87b696ea3c016e16d70accd7bde0d21
Karol Furmanski <karolfur1995@gmail.com>
2017-10-17 20:29:25 +0200

    Add .war file, .gitignore file, readme file and .sh script

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # FilmotekaCentralService
+
+FilmotekaCentralService is a script to make it easier to run the [Filmoteka](https://github.com/LorakPL/Filmoteka) project on your Linux device (e.g. single-board computer - Raspberry Pi).
+
+Once you run the script on your device, it will be checked:
+- **MySQL**
+- **Tomcat**
+- **Java**
+
+If it turns out that you do not have these tools, the script will install them automatically.
+
+## Getting Started
+If you are in the folder with the startService.sh file just enter the command:
+
+```
+sudo ./startService.sh
+```
+
+
+
+## Tools
+- **Java** - version 8
+- **Tomcat** - version 7
+- **MySQL** - newest version
+
+## Authors
+- **Karol Furmański**
+- **Arkadiusz Lalo**
---
+++ startService.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+SERVICE_WAR=filmoteka.war
+TOMCAT7_HOME=/home/pi/apache-tomcat-7.0.81
+MYSQL_ROOT_PASS=root
+
+#Check Java
+echo 'Check Java...' >&2
+if ! [ -x "$(command -v java)" ]; then
+  echo 'Install Java.' >&2
+  sudo apt-get install -y oracle-java8-jdk
+fi
+echo 'Java OK.' >&2
+
+#Check Tomcat7
+echo 'Check Tomcat7...' >&2
+if [[ ! -e $TOMCAT7_HOME ]]; then
+  echo 'Install Tomcat7.' >&2
+  wget http://ftp.ps.pl/pub/apache/tomcat/tomcat-7/v7.0.81/bin/apache-tomcat-7.0.81.tar.gz
+  tar xzf apache-tomcat-7.0.81.tar.gz -C ~/
+  rm -f apache-tomcat-7.0.81.tar.gz
+fi
+echo 'Tomcat7 OK.' >&2
+
+#Check mysql
+echo 'Check mysql...' >&2
+if ! [ -x "$(command -v mysql)" ]; then
+  echo 'Install mysql.' >&2
+  echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASS" | sudo debconf-set-selections
+  echo "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASS" | sudo debconf-set-selections
+  sudo apt-get install -y mysql-server
+fi
+echo 'mysql OK.' >&2
+
+if ! [ -x "$(ps aux | grep tomcat)" ]; then
+  echo "Stop tomcat" >&2
+  bash $TOMCAT7_HOME/bin/catalina.sh stop
+fi
+
+echo "Setting up the site" >&2
+cp $SERVICE_WAR $TOMCAT7_HOME/webapps/$SERVICE_WAR
+
+echo "Start tomcat" >&2
+bash $TOMCAT7_HOME/bin/catalina.sh start
