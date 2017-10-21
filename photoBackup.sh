006c30bb1124ae821eb07a101ddd3d6b18aae52e
Karol Furmanski <karolfur1995@gmail.com>
2017-10-21 08:56:13 +0200

    New photoBackup script

--- photoBackup.sh
+++ photoBackup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 #Purpose = Backup of photos in Filmoteka
+
 #START
+
 DATEANDTIME=`%Y-%m-%d_%H-%M-%S`
 FILENAME=backup-filmoteka-$DATEANDTIME.tar.gz
-SRCDIR=~/apache-tomcat-7.0.81/webapps/filmoteka                   # Location of Important Data Directory (Source of backup).
-DESDIR=~/Desktop/Backup            # Destination of backup file.
-tar -zcvf $DESDIR/$FILENAME $SRCDIR
+SRCDIR=/root/apache-tomcat-7.0.81/webapps/filmoteka     # Location of Important Data Directory (Source of backup).
+DESDIR=/home/pi/Documents/Filmoteka/Backup              # Destination of backup file.
+tar -cpzf $DESDIR/$FILENAME $SRCDIR
+
 #END
