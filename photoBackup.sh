1d4262e9a0afd21603a8ecebf026a6ffcbbf25e5
Karol Furmanski <karolfur1995@gmail.com>
2017-10-23 16:12:46 +0200

    new path to images in photoBackup.sh

--- photoBackup.sh
+++ photoBackup.sh
@@ -6,8 +6,8 @@
 
 DATEANDTIME=`date +%Y-%m-%d_%H-%M-%S`
 FILENAME=backup-filmoteka-$DATEANDTIME.zip
-SRCDIR=/root/apache-tomcat-7.0.81/webapps/filmoteka     # Location of Important Data Directory (Source of backup).
-DESDIR=/home/pi/Documents/Filmoteka/Backup              # Destination of backup file.
+SRCDIR=/root/apache-tomcat-7.0.81/webapps/filmoteka/images     	# Location of Important Data Directory (Source of backup).
+DESDIR=/home/pi/Documents/Filmoteka/Backup              	# Destination of backup file.
 zip -r $DESDIR/$FILENAME $SRCDIR
 
 #END
